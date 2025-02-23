88fb07402dfdb2b28c3e61ca8165648ae4d89356
david-why <david_why@outlook.com>
2025-02-23 22:00:18 +0800

    Make basic website layout

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,74 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { onMounted, ref, watch } from 'vue'
+
+// refs to elements
+const fileInput = ref({} as HTMLInputElement)
+const canvas = ref({} as HTMLCanvasElement)
+
+// settings
+const scale = ref(8)
+const baseImage = ref<HTMLImageElement>()
+
+// event handlers
+
+function onFileSelected() {
+  const file = fileInput.value.files?.[0]
+  if (!file) return
+  const img = new Image()
+  img.onload = () => {
+    baseImage.value = img
+  }
+  img.src = URL.createObjectURL(file)
+}
+
+onMounted(() => {
+  // load demo image
+  const img = new Image()
+  img.src = 'wires.bmp'
+  img.onload = () => {
+    baseImage.value = img
+  }
+})
+
+// canvas functions
+function initCanvas() {
+  const img = baseImage.value
+  if (!img) return
+
+  // this must come before the getContext call, otherwise the image will be blurry
+  canvas.value.width = img.naturalWidth * scale.value
+  canvas.value.height = img.naturalHeight * scale.value
+
+  const ctx = canvas.value.getContext('2d')
+  if (!ctx) return
+
+  ctx.imageSmoothingEnabled = false
+  ctx.drawImage(img, 0, 0, img.width * scale.value, img.height * scale.value)
+}
+
+// watchers
+
+watch([baseImage, scale], initCanvas)
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>Bitmap Logic Simulator</h1>
+  <p>
+    Originally created by
+    <a href="https://realhet.wordpress.com/2015/09/02/bitmap-logic-simulator/">realhet</a>
+    · Website &amp; demos created by
+    <a href="https://github.com/david-why/bmplogicsim">david-why</a>
+  </p>
+  <div>
+    <input type="file" accept=".bmp" @input="onFileSelected" ref="fileInput" />
+  </div>
+  <div>
+    <input type="range" min="0.1" max="10" step="0.1" v-model="scale" />
+    <span>{{ scale }}</span>
+  </div>
+  <div>
+    <canvas ref="canvas"></canvas>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
